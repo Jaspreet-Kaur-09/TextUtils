17a37e1c11eb57b7c122402c01f0466f4f08ae3c
Jaspreet Kaur <140949288+Jaspreet-Kaur-09@users.noreply.github.com>
2023-08-03 18:26:47 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,15 +6,33 @@ export default function TextForm(props) {
 const handleUpClick=()=>{
     console.log("Uppercase is clicked");
     let newText=text.toUpperCase();
-    setText(newText)
+    setText(newText);
+    props.showAlert("converted to Uppercase","success");
 }
+
 const handleLowClick=()=>{
   console.log("Lowercase is clicked");
   let newText=text.toLowerCase();
-  setText(newText)
+  setText(newText);
+  props.showAlert("Converted to Lowecase","sucess");
 }
+
 const handleResetClick=()=>{
   setText('');
+  props.showAlert("Reset text","sucess");
+}
+
+const handleCopy=()=>{
+  var text=document.getElementById('myBox');
+  text.select();
+  navigator.clipboard.writeText(text.value);
+  props.showAlert("Copied","sucess");
+}
+
+const handleExtraSpace=()=>{
+  let newText=text.split(/[ ]+/);
+  setText(newText.join(" "));
+  props.showAlert("Extra spaces removed","sucess");
 }
 
 const handleOnChange=(event)=>{
@@ -29,21 +47,23 @@ return (
 <>
 
 <div className="container">
-<div className="mb-3">
+<div className="mb-3" style={{color:props.mode==='light'?'black':'white'}}>
 <h1>{props.heading}</h1>
-<textarea className="form-control" onChange={handleOnChange} value={text} id="myBox" rows="8"></textarea>
+<textarea className="form-control" onChange={handleOnChange} value={text} style={{backgroundColor:props.mode==='light'?'white':'grey',color:props.mode==='light'?'black':'white'}} id="myBox" rows="8"></textarea>
 <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
 <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to Lowercase</button>
 <button className="btn btn-primary mx-1" onClick={handleResetClick}>Reset</button>
+<button className="btn btn-primary mx-1" onClick={handleCopy}>Copy</button>
+<button className="btn btn-primary mx-1" onClick={handleExtraSpace}>Remove Extra Space</button>
 </div>
 </div>
 
-<div className="container my-4">
+<div className="container my-4"style={{color:props.mode==='light'?'black':'white'}}>
   <h1>Your text summary</h1>
   <p>{text.split(" ").length} words and {text.length} characters</p>
   <p>{0.008*text.split(" ").length} Minutes Read</p>
   <h2>Preview</h2>
-  <p>{text}</p>
+  <p>{text.length>0?text:"Enter text to preview"}</p>
 </div>
 </>
 
